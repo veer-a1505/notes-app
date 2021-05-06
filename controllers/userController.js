23299ad7ba2e415690c20093d7190ef2132a6959
veera_1505 <veerasaraha@gmail.com>
2021-05-06 13:50:36 +0530

    user signup and signin feature

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,14 +2,52 @@ import User from '../models/userModel.js'
 
 export const createUser = async (req, res, next) => {
   try {
-    const body = req.body
-
-    console.log(body)
-
-    res.status(200).json({ body })
-  } catch (error) {}
+    const user = await User.create(req.body)
+    return res.status(200).json({
+      success: 'success',
+      data: {
+        user,
+      },
+    })
+  } catch (error) {
+    return res.status(400).json({
+      success: 'error',
+      data: {
+        message: error.message,
+      },
+    })
+  }
 }
 
 export const loginUser = async (req, res, next) => {
-  res.status(200).json({ message: 'User login' })
+  try {
+    const { email, password } = req.body
+
+    if (!email || !password) {
+      return res.status(400).json({
+        status: 'error',
+        data: {
+          message: 'Please provide email and password',
+        },
+      })
+    }
+
+    const user = await User.findOne({ email })
+
+    if (!user || !(await user.comparePassword(password, user.password))) {
+      return res.status(401).json({
+        status: 'error',
+        data: {
+          message: 'Incorrect email or password',
+        },
+      })
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        message: 'Successfully Logged In..!',
+      },
+    })
+  } catch (error) {}
 }
--- models/userModel.js
+++ models/userModel.js
@@ -1,27 +1,42 @@
 import mongoose from 'mongoose'
-
+import bcrypt from 'bcryptjs'
 const { Schema } = mongoose
 
 const userSchema = new Schema({
   username: {
     type: String,
-    required: true,
+    required: [true, 'please provide username'],
   },
   email: {
     type: String,
-    required: true,
+    required: [true, 'please provide email'],
     unique: true,
     index: true,
   },
   password: {
     type: String,
-    required: true,
+    required: [true, 'please provide password'],
   },
   accountCreatedAt: {
     type: Date,
     default: Date.now,
   },
 })
+
+// Hash password before saving document
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next()
+  this.password = await bcrypt.hash(this.password, 12)
+  next()
+})
+
+// Verify passowrd if it matches or not
+userSchema.methods.comparePassword = async function (
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword)
+}
 
 const User = mongoose.model('User', userSchema)
 
