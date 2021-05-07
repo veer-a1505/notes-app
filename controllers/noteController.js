c1217561c547b54886bdb3da6df155d7acd53847
veera_1505 <veerasaraha@gmail.com>
2021-05-07 14:55:01 +0530

    added notes update and delete routes & controller

--- controllers/noteController.js
+++ controllers/noteController.js
@@ -4,7 +4,7 @@ export const createNote = async (req, res, next) => {
   try {
     const notes = await Note.create(req.body)
 
-    res.status(200).json({
+    res.status(201).json({
       status: 'success',
       data: {
         notes,
@@ -12,6 +12,95 @@ export const createNote = async (req, res, next) => {
     })
   } catch (error) {
     return res.status(400).json({
+      status: 'error',
+      data: {
+        message: error.message,
+      },
+    })
+  }
+}
+
+export const getNotesByID = async (req, res, next) => {
+  try {
+    const note = await Note.findById(req.params)
+
+    if (!note) {
+      return res.status(404).json({
+        status: 'error',
+        data: {
+          message: `Notes not found with this ${req.params._id}`,
+        },
+      })
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        note,
+      },
+    })
+  } catch (error) {
+    res.status(400).json({
+      status: 'error',
+      data: {
+        message: error.message,
+      },
+    })
+  }
+}
+
+export const updateNoteByID = async (req, res, next) => {
+  try {
+    const updatedNotes = await Note.findByIdAndUpdate(req.params, req.body, {
+      new: true,
+    })
+
+    if (!updatedNotes) {
+      return res.status(404).json({
+        status: 'error',
+        data: {
+          message: `Notes not found with this ${req.params._id}`,
+        },
+      })
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        updatedNotes,
+      },
+    })
+  } catch (error) {
+    res.status(400).json({
+      status: 'error',
+      data: {
+        message: error.message,
+      },
+    })
+  }
+}
+
+export const deleteNoteByID = async (req, res, next) => {
+  try {
+    const notes = await Note.findOneAndDelete(req.params)
+
+    if (!notes) {
+      return res.status(404).json({
+        status: 'error',
+        data: {
+          message: `Notes not found with this ${req.params._id}`,
+        },
+      })
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        message: 'Succssfully deleted',
+      },
+    })
+  } catch (error) {
+    res.status(400).json({
       status: 'error',
       data: {
         message: error.message,
--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -4,4 +4,8 @@ import * as noteController from './../controllers/noteController.js'
 
 noteRoutes.post('/addnote', noteController.createNote)
 
+noteRoutes.get('/getNoteByID/:_id', noteController.getNotesByID)
+noteRoutes.patch('/updateNoteByID/:_id', noteController.updateNoteByID)
+noteRoutes.delete('/deleteNoteByID/:_id', noteController.deleteNoteByID)
+
 export default noteRoutes
